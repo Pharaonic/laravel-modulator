019493de5bddbf90907e9576cd562224a3ab409d
Moamen Eltouny <raggigroup@gmail.com>
2021-12-17 12:18:47 +0200

    add more helpers

--- src/helpers.php
+++ src/helpers.php
@@ -1,23 +1,39 @@
 <?php
 
-// Modules Path
-if (! function_exists('modules_path')) {
-    function modules_path()
+use Illuminate\Support\Str;
+
+
+// Get Files List
+if (!function_exists('getFiles')) {
+    function getFiles(string $dir)
     {
-        return module_path();
+        if (!file_exists($dir)) return [];
+        return array_values(array_diff(scandir($dir), ['.', '..']));
     }
 }
 
 // Module Path
-if (! function_exists('module_path')) {
-    function module_path(?string $module = null)
+if (!function_exists('module_path')) {
+    function module_path(?string $module = null, ?string $extra = null)
     {
-        return app_path('Modules' . ($module ? DIRECTORY_SEPARATOR . $module : null));
+        if (!$module) return app_path('Modules');
+        if ($extra) $extra = implode(DIRECTORY_SEPARATOR, explode('/', $extra));
+
+        return app_path('Modules') . DIRECTORY_SEPARATOR . $module . DIRECTORY_SEPARATOR . $extra;
+    }
+}
+
+// Modules List
+if (!function_exists('modules')) {
+    function modules()
+    {
+        $dir = module_path();
+        return file_exists($dir) ? getFiles($dir) : [];
     }
 }
 
 // Config Path
-if (! function_exists('module_config_path')) {
+if (!function_exists('module_config_path')) {
     function module_config_path(string $module)
     {
         return module_path($module) . DIRECTORY_SEPARATOR . 'config';
@@ -25,7 +41,7 @@ if (! function_exists('module_config_path')) {
 }
 
 // Database Path
-if (! function_exists('module_database_path')) {
+if (!function_exists('module_database_path')) {
     function module_database_path(string $module)
     {
         return module_path($module) . DIRECTORY_SEPARATOR . 'database';
@@ -33,9 +49,17 @@ if (! function_exists('module_database_path')) {
 }
 
 // Resources Path
-if (! function_exists('module_resource_path')) {
+if (!function_exists('module_resource_path')) {
     function module_resource_path(string $module)
     {
         return module_path($module) . DIRECTORY_SEPARATOR . 'resources';
+    }
+}
+
+// Convert studly to slug
+if (!function_exists('studlyToSlug')) {
+    function studlyToSlug(string $text)
+    {
+        return Str::slug(strtolower(trim(preg_replace('/[A-Z-]/', ' $0', $text))));
     }
 }
